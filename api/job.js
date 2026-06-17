be96eb81656f43877d30a21a1b09d47442caf271
squarepusher34 <132813276+squarepusher34@users.noreply.github.com>
2026-06-17 15:10:06 +0300

    Update job.js

--- api/job.js
+++ api/job.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
-  }
+  } 
 
   try {
     const response = await fetch("https://api.cloudconvert.com/v2/jobs", {
